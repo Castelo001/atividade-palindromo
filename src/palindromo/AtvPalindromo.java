93724fa111ebf54ea312305aaac2b4b7179b554e
Fernando J.r <castelobrancomoura@hotmail.com>
2026-01-19 14:43:42 -0300

    Numero de vogais e consoantes

--- src/palindromo/AtvPalindromo.java
+++ src/palindromo/AtvPalindromo.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class AtvPalindromo {
 
 	public static void main(String[] args) {
-		int vogais =0;
-		int consoantes =0;
+		int vogais = 0;// criar variaveis para contar as vogais e consoantes
+		int consoantes = 0;
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Digite uma palavra");
 		String palavra = entrada.nextLine();
@@ -14,14 +14,25 @@ public class AtvPalindromo {
 		String pInvertida = rv.reverse().toString();
 		System.out.printf("Palavra invertida:%s\n", pInvertida);
 		if (palavra.equalsIgnoreCase(pInvertida)) {
+
 			System.out.println("É palindrono");
 		} else {
-			System.out.println("Não é palindromo");
+			System.out.println("Não é palindromo.");
 		}
-		System.out.printf("O numero de letras da palavra é: \n %s letras", palavra.length());
-		for (int i = 0; i < palavra.length(); i++) { palavra.charAt(i);	}
-		
+		System.out.printf("O numero de letras da palavra é:%s\n", palavra.length());
+		for (int i = 0; i < palavra.length(); i++) {
+			Character letra = palavra.charAt(i);
+			letra = Character.toLowerCase(letra);
+			if (Character.isLetter(letra)) {
+				if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+					vogais++;
+				} else {
+					consoantes++;
+				}
+			}
+		}
+		System.out.printf("Numero de consoantes:%d\n", consoantes);
+		System.out.printf("Numero de vogais:%d\n", vogais);
 		entrada.close();
-
 	}
 }
