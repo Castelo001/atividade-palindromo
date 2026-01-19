a4bf64d4b04ea6c0fba76f85be4506d78df45e42
Fernando J.r <castelobrancomoura@hotmail.com>
2026-01-19 09:47:51 -0300

    Atividade separacao letras

--- src/palindromo/AtvPalindromo.java
+++ src/palindromo/AtvPalindromo.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class AtvPalindromo {
 
 	public static void main(String[] args) {
+		int vogais =0;
+		int consoantes =0;
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Digite uma palavra");
 		String palavra = entrada.nextLine();
@@ -16,7 +18,10 @@ public class AtvPalindromo {
 		} else {
 			System.out.println("Não é palindromo");
 		}
-			System.out.printf("O numero de letras da palavra é:%s",palavra.length());
-			entrada.close();
+		System.out.printf("O numero de letras da palavra é: \n %s letras", palavra.length());
+		for (int i = 0; i < palavra.length(); i++) { palavra.charAt(i);	}
+		
+		entrada.close();
+
 	}
 }
