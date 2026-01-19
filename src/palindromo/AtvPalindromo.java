a9419cdaae92cf3b0e3f8c30d1d4a515504ff04c
Fernando J.r <castelobrancomoura@hotmail.com>
2026-01-18 22:46:43 -0300

    primeiro commit atvPalindromo

---
+++ src/palindromo/AtvPalindromo.java
@@ -0,0 +1,23 @@
+package palindromo;
+
+import java.util.Scanner;
+
+public class AtvPalindromo {
+
+	public static void main(String[] args) {
+Scanner entrada = new Scanner(System.in);
+System.out.println("Digite uma palavra");
+String palavra = entrada.nextLine();
+StringBuilder rv = new StringBuilder(palavra);
+String pInvertida = rv.reverse().toString();
+System.out.printf("Palavra invertida:%s\n",pInvertida);
+if(palavra.equalsIgnoreCase(pInvertida)) {
+	System.out.println("É palindrono");
+}else{
+System.out.println("Não é palindromo");
+entrada.close();
+
+	}
+
+}
+}
